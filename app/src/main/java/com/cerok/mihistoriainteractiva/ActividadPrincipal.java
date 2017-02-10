74182c2fe787a9db6ec051263ce057f9ef403ac1
alejandro <alejo.carvajal03@gmail.com>
2017-02-09 22:57:26 -0500

    Se crea la actividad con imagenes, campos, botones y acciones.

--- app/src/main/java/com/cerok/mihistoriainteractiva/ActividadPrincipal.java
+++ app/src/main/java/com/cerok/mihistoriainteractiva/ActividadPrincipal.java
@@ -2,12 +2,26 @@ package com.cerok.mihistoriainteractiva;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class ActividadPrincipal extends AppCompatActivity {
-
+    private EditText mCampoNombre;
+    private Button mBotonInicio;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_actividad_principal);
+        mCampoNombre = (EditText) findViewById(R.id.NombreeditText);
+        mBotonInicio = (Button) findViewById(R.id.botonInicio);
+        mBotonInicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nombre = mCampoNombre.getText().toString();
+                Toast.makeText(ActividadPrincipal.this,nombre,Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
